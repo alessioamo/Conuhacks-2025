0bd37b810572b9f1fc20efbc29d730e93ce64f7b
kostaaaaaa <deanfountas@gmail.com>
2025-02-01 15:50:22 -0500

    Scripts and Scenes :)

---
+++ Conuhacks 2025/Assets/Scripts/ClickInteractable.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickInteractable : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject) {
+                OnClick(hit.collider.gameObject.tag);
+            }
+        }
+    }
+
+    public void OnClick(string tag) {
+        if (tag == "Darts") {
+            SceneManager.LoadScene(2);
+        }
+        else if (tag == "Wanted") {
+            SceneManager.LoadScene(3);
+        }
+        else if (tag == "Blackjack") {
+            SceneManager.LoadScene(4);
+        }
+        else if (tag == "Boss") {
+            SceneManager.LoadScene(5);
+        }
+        else if (tag == "Bar") {
+            SceneManager.LoadScene(6);
+        }
+    }
+}

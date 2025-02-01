27c0d022df9b04cfc68f7587e3954f4ebc1e63ac
alessioamo <80137378+alessioamo@users.noreply.github.com>
2025-02-01 11:47:42 -0500

    Initial Push

---
+++ Conuhacks 2025/Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int value;
+    public int suit;
+    public Sprite imageBack;
+    public bool isAce;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
